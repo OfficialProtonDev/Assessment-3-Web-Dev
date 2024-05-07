0a675ab20d7dc61aea78953734ef3164755a50c6
OfficialProtonDev <98558514+OfficialProtonDev@users.noreply.github.com>
2024-05-08 11:26:10 +1200

    init

---
+++ js/owl-carousel.js
@@ -0,0 +1,11 @@
+$('.owl-carousel').owlCarousel({
+    items: 1,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 7000,
+    autoplaySpeed: 2000,
+    autoplayHoverPause: false,
+    nav: false,
+    dots: false,
+    autoHeight: true
+})
